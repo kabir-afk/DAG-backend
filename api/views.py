b1bdf0922167689f9e0349c1977f41f524d4e963
Kabir Singh <kabirsingh12b@gmail.com>
2025-12-07 03:41:39 +0530

    feat : checked whether graph is cyclic or not

--- api/views.py
+++ api/views.py
@@ -1,33 +1,61 @@
 from rest_framework.response import Response
 from rest_framework.views import APIView
-# Create your views here.
 
 class ReadRoot(APIView):
     def get(self,req):
         return Response({'Ping': 'Pong'})
 
 class ParsePipelineView(APIView):
+    def dfsCheck(self, node, adj, visited, pathVisited):
+        visited[node] = 1
+        pathVisited[node] = 1
+        
+        for neighbor in adj[node]:
+            if visited[neighbor] == 0:
+                if self.dfsCheck(neighbor, adj, visited, pathVisited) == True:
+                    return True
+            elif pathVisited[neighbor] == 1:
+                return True
+
+        pathVisited[node] = 0
+        return False
+
     def post(self,req):
         nodes,edges = req.data.values()
-        # for calculating total number of nodes
         nodeSet = set()
         edgeSet = set()
+        
         for edge in edges:
             nodeSet.add(edge['source'])
             nodeSet.add(edge['target'])
-        # to avoid duplicate edges
+        
         for edge in edges:
             edge_key = (edge['source'], edge['target'])
             edgeSet.add(edge_key)
         
-        # creating adjacency list
         adj = {}
         for node in nodeSet:
             adj[node] = set()
-        for source,target in edgeSet:
-            if source not in adj:
-                adj[source] = set()
+        
+        for source, target in edgeSet:
             adj[source].add(target)
+        
         print(adj)
-
-        return Response({'number of nodes': len(nodeSet), 'number of edges': len(edgeSet)})
+        
+        visited = {}
+        pathVisited = {}
+        for node in adj:
+            visited[node] = 0
+            pathVisited[node] = 0
+        
+        is_cyclic = False
+        for node in visited:
+            if visited[node] == 0:
+                if self.dfsCheck(node, adj, visited, pathVisited) == True:
+                    is_cyclic = True
+                    break
+        
+        is_dag = not is_cyclic
+        
+        print(visited)
+        return Response({'num_nodes': len(nodeSet), 'num_edges': len(edgeSet), "is_dag": is_dag})
