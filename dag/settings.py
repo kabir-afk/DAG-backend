90ceafd241201be84f7c276c9ef45af897e0f044
Kabir Singh <kabirsingh12b@gmail.com>
2025-12-06 18:53:06 +0530

    update : settings
    Placed scert key in env variables
    
    added installed apps

--- dag/settings.py
+++ dag/settings.py
@@ -11,6 +11,9 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+from environ import Env
+env= Env()
+env.read_env()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-$$xeu+z+81d5c6rkg7odf1u8dtquh-^l&6sm)x7i%vj5ya-hon'
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -37,6 +40,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'restframework',
+    'api'
 ]
 
 MIDDLEWARE = [
