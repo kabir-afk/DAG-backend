9b0fc5009c2c08b4e1282f9ef7b0d8f18b83dc92
Kabir Singh <kabirsingh12b@gmail.com>
2025-12-06 19:41:40 +0530

    added basic views

--- api/views.py
+++ api/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
-
+from rest_framework.response import Response
+from rest_framework.views import APIView
 # Create your views here.
+
+class ReadRoot(APIView):
+    def get(self,req):
+        return Response({'Ping': 'Pong'})
+
+class ParsePipelineView(APIView):
+    def post(self,req):
+        return Response({'status': 'parsed'})
